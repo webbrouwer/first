e480ea7da64a6c40343a3bf54af6c40b501adac7
webbrouwer <mickey@webbrouwer.com>
2018-03-01 21:22:40 +0100

    create register form

---
+++ register.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title>Register</title>
+</head>
+<body>
+    <h1><a href="index.php">Homepage</a></h1>
+
+    <h2>Register</h2>
+
+    <form action="register.php" method="POST">
+        Username: <input type="text" name="username" required="required"> <br>
+        Password: <input type="password" name="password" required="required"> <br>
+        <input type="submit" value="Register">
+    </form>
+
+
+    <a href="login.php">Login</a>
+</body>
+</html>
