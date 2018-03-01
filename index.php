cbe7d6894af64f3c684097ff5784f32b8553e536
webbrouwer <mickey@webbrouwer.com>
2018-03-01 21:22:19 +0100

    setup links to register and login

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
     <title>Home</title>
 </head>
 <body>
-    <h1>Homepage</h1>
+    <h1><a href="index.php">Homepage</a></h1>
+
+    <a href="register.php">Register</a>
+    <a href="login.php">Login</a>
 </body>
 </html>
