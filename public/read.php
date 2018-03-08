fd49ab90416e2f670ed15fed4cd06fc3d956acb3
webbrouwer <mickey@webbrouwer.com>
2018-03-08 01:02:49 +0100

    create read page

--- public/read.php
+++ public/read.php
@@ -1,13 +1,102 @@
+<?php 
+
+if (isset($_POST['submit'])) {
+
+    try {
+
+        require "../config.php";
+        require "../functions.php";
+
+        $connection = new PDO($dsn, $username, $password, $options);
+        // fetch data code here
+
+        $sql = "SELECT * 
+                FROM users 
+                WHERE location = :location";
+
+        $location = $_POST['location'];
+
+        $statement = $connection->prepare($sql);
+        $statement->bindParam(':location', $location, PDO::PARAM_STR);
+        $statement->execute();
+
+        $result = $statement->fetchAll();
+    }
+
+    catch(PDOExeption $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+    
+}
+
+
+?>
+
+
 <?php include "templates/header.php"; ?>
+
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="create.php">Create</a></li>
+        <li><a href="read.php">Read</a></li>
+        <li><a href="update.php">Update</a></li>
+        <li><a href="delete.php">Delete</a></li>
+    </ul>
+
+    <?php 
+    if (isset($_POST['submit'])) {
+        if ($result && $statement->rowCount() > 0) {
+            // Open table
+            ?>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Username</th>
+                        <th>Password</th>
+                    </tr>
+                </thead>
+                <tbody>
+    
+            <?php
+            foreach ($result as $row) {
+                // Table content
+                ?>
+
+                    <tr>
+                        <td><?php echo escape($row['id']); ?></td>
+                        <td><?php echo escape($row['username']); ?></td>
+                        <td><?php echo escape($row['password']); ?></td>
+                    </tr>
+
+                <?php
+            }
+            // Close table
+            ?>
+
+            </tbody>
+            </table>
+
+            <?php
+        }
+        else {
+            // No results
+            ?>
+
+                <blockquote>No results find for <?php echo escape($_POST['location']); ?></blockquote>
+
+            <?php
+        }
+    }
+    ?>
 
     <h2>Find user for location</h2>
 
-    <form action="read.php" method="POST">
-        <label for="location">Username</label>
+    <form method="post">
+        <label for="location">Location</label>
         <input type="text" name="location" id="location" required="required"> <br>
-        <input type="submit" value="View Results">
+        <input type="submit" name="submit" value="Submit">
     </form>
 
-    <a href="create.php">Create</a>
-    
 <?php include "templates/footer.php"; ?>
