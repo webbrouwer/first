49d9d2782019afca5356491595d226ac64f23065
webbrouwer <mickey@webbrouwer.com>
2018-03-08 01:03:05 +0100

    set up update page, ready for dev

---
+++ public/update.php
@@ -0,0 +1,15 @@
+<?php include "templates/header.php"; ?>
+    
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="create.php">Create</a></li>
+        <li><a href="read.php">Read</a></li>
+        <li><a href="update.php">Update</a></li>
+        <li><a href="delete.php">Delete</a></li>
+    </ul>
+
+    <h2>Update user</h2>
+
+    <em>Ready for development</em>
+
+<?php include "templates/footer.php"; ?>
