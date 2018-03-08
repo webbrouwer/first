737fb69dc8d5b4626200368fa4252b05f9a2c7f7
webbrouwer <mickey@webbrouwer.com>
2018-03-08 01:02:31 +0100

    add menu to home

--- public/index.php
+++ public/index.php
@@ -1,8 +1,11 @@
     <?php include "templates/header.php"; ?>
     
     <ul>
+        <li><a href="index.php">Home</a></li>
         <li><a href="create.php">Create</a></li>
         <li><a href="read.php">Read</a></li>
+        <li><a href="update.php">Update</a></li>
+        <li><a href="delete.php">Delete</a></li>
     </ul>
 
     <?php include "templates/footer.php"; ?>
