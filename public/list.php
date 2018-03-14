bd4e850b3abb7ec7e9a87aebe7adf11389c3b371
webbrouwer <mickey@webbrouwer.com>
2018-03-14 09:26:20 +0100

    set up list template

---
+++ public/list.php
@@ -0,0 +1,15 @@
+<?php include "templates/header.php"; ?>
+
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="create.php">Create</a></li>
+        <li><a href="read.php">Read</a></li>
+        <li><a href="update.php">Update</a></li>
+        <li><a href="delete.php">Delete</a></li>
+    </ul>
+
+    <h2>List Users</h2>
+
+    <em>Ready for dev</em>
+
+<?php include "templates/footer.php"; ?>
