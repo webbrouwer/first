ded47207ef1d006cf9fc424c0fafeefe3d4eec0c
webbrouwer <mickey@webbrouwer.com>
2018-03-21 00:14:43 +0100

    refactor menu and set up login

--- public/index.php
+++ public/index.php
@@ -1,13 +1,64 @@
-    <?php include "templates/header.php"; ?>
-    
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="create.php">Create</a></li>
-        <li><a href="read.php">Read</a></li>
-        <li><a href="update.php">Update</a></li>
-        <li><a href="delete.php">Delete</a></li>
-        <li><a href="list.php">List</a></li>
-        <li><a href="search.php">Search</a></li>
-    </ul>
+<?php 
 
-    <?php include "templates/footer.php"; ?>
+if (isset($_POST['submit'])) {
+
+    try {
+
+        require "../config.php";
+        require "../functions.php";
+
+        $connection = new PDO($dsn, $username, $password, $options);
+
+        $sql = "SELECT id 
+                FROM users 
+                WHERE username = :username
+                AND password = :password";        
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $statement = $connection->prepare($sql);
+        $statement->bindParam("username", $username, PDO::PARAM_STR);
+        // $enc_password = hash('sha256', $password);
+        $statement->bindParam("password", $password, PDO::PARAM_STR);
+        $statement->execute();
+
+        if ($statement->rowCount() > 0) {
+        $result = $statement->fetchAll();
+        } else {
+            $error = "Username or password incorrect";
+        }
+
+    } catch (PDOException $e) {
+        exit($e->getMessage());
+    }    
+}
+
+?>
+
+<?php include "templates/header.php"; ?>
+
+    <?php 
+        if (isset($_POST['submit'])) {
+            if ($result && $statement->rowCount() > 0) {
+                foreach ($result as $row) {
+                    $id = $row['id'];
+                }
+                $_SESSION['id'] = $id;
+                header("Location: profile.php");
+            } else 
+                echo $error;
+        }
+        ?>
+
+    <h2>Login</h2>
+
+    <form method="post">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" required="required"> <br>
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" required="required"> <br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+
+<?php include "templates/footer.php"; ?>
