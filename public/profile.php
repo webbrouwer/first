b4be03e5dbce48fff9328ec86a6a7e6c412bff98
webbrouwer <mickey@webbrouwer.com>
2018-03-26 23:39:34 +0200

    check if logged in, if so show logout

--- public/profile.php
+++ public/profile.php
@@ -1,8 +1,92 @@
+<?php 
+
+require_once("check-login.php");
+
+if (isset($_SESSION['id'])) {
+
+    try {
+
+        require "../config.php";
+        require "../functions.php";
+
+        $connection = new PDO($dsn, $username, $password, $options);
+        // fetch data code here
+
+        $sql = "SELECT * 
+                FROM users 
+                WHERE id = :id";
+
+        $id = $_SESSION['id'];
+
+        $statement = $connection->prepare($sql);
+        $statement->bindParam(':id', $id, PDO::PARAM_STR);
+        $statement->execute();
+
+        $result = $statement->fetchAll();
+    }
+
+    catch(PDOExeption $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    } 
+}
+
+?>
+
 <?php include "templates/header.php"; ?>
     
     <em>Create profilepage</em>
 
     <?php $session_id = $_SESSION['id']; ?>
     <?php echo $session_id; ?>
+
+<?php 
+    if (isset($_POST['submit'])) {
+        if ($result && $statement->rowCount() > 0) {
+            // Open table
+            ?>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Username</th>
+                        <th>Password</th>
+                        <th>Location</th>
+                    </tr>
+                </thead>
+                <tbody>
+    
+            <?php
+            foreach ($result as $row) {
+                // Table content
+                ?>
+
+                    <tr>
+                        <td><?php echo escape($row['id']); ?></td>
+                        <td><?php echo escape($row['username']); ?></td>
+                        <td><?php echo escape($row['password']); ?></td>
+                        <td><?php echo escape($row['location']); ?></td>
+                    </tr>
+
+                <?php
+            }
+            // Close table
+            ?>
+
+            </tbody>
+            </table>
+
+            <?php
+        }
+        else {
+            // No results
+            ?>
+
+                <blockquote>No results find</blockquote>
+
+            <?php
+        }
+    }
+    ?>    
 
 <?php include "templates/footer.php"; ?>
--- public/templates/footer.php
+++ public/templates/footer.php
@@ -1,2 +1,9 @@
+
+<footer>
+    <?php if(isset($_SESSION['id'])) { ?>
+        <a href="../logout.php">Logout</a>
+    <?php } ?>
+</footer>
+
 </body>
 </html>
